2ca884fff4a83fa515e33c8ecb135b0e602718fe
Your Name <andangsaefullah4@gmail.com>
2020-10-19 16:02:08 +0700

    Add project files.

---
+++ Employee_032/Models/EmployeeContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Employee_032.Models
+{
+    public class EmployeeContext : DbContext
+    {
+        public EmployeeContext(DbContextOptions<EmployeeContext> options) :
+        base(options)
+        { }
+        public DbSet<Employees> Employees { get; set; }
+    }
+}
---
+++ Employee_032/Startup.cs
@@ -0,0 +1,15 @@
+﻿using Employee_032.Models;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Employee_032.Startup))]
+namespace Employee_032
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            services.AddDbContext<EmployeeContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DevConnection")));
+        }
+    }
+}
